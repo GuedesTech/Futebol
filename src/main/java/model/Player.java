e9ed2e4933ccd33d2013cafef95a87b3842d7246
GuedesTech <guilhermeguedes350@gmail.com>
2025-10-31 18:37:18 -0300

    refactor(model) Use Lombok annotations to simplify Player class.

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -7,11 +7,19 @@ import lombok.*;
 @NoArgsConstructor // Constructor
 @ToString // toString
 
-public class Player extends Team {
+public class Player {
     private int id;
     private String name; // nome
     private String position; // posicao
     private int jerseyNumber; // numeroCamisa
     private int age; // idade
     private int teamId; // idTime (FK for Team)
+
+    public Player (String name, String position, int jerseyNumber, int age, int teamId){
+        this.name = name;
+        this.position = position;
+        this.jerseyNumber = jerseyNumber;
+        this.age = age;
+        this.teamId = teamId;
+    }
 }
